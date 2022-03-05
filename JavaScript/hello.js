5bc710f65c6cf8acaa2a41db216c3a2e82a895dd
shubham409 <sm091274@gmail.com>
2022-03-05 22:09:09 +0530

    JavaScript Commit

---
+++ JavaScript/hello.js
@@ -0,0 +1,100 @@
+console.log("hello world")
+
+let val = 8
+let dec = 8888888888888888888888.99999999999999999999999999999999999999
+let c ='j'
+let ch ='h'
+let st = "string"
+let s = 'jaldjslfjklsjfll'
+let ar = [22,3,3,4,4,4,4,]
+let dct ={
+    'val':'this',
+    "hello":"brother"
+}
+
+let bool = true;
+
+console.log(typeof val)
+console.log(typeof c)
+console.log(typeof ch)
+console.log(typeof st)
+console.log(typeof s)
+console.log(typeof dct)
+console.log(typeof bool)
+console.log(typeof dec)
+
+
+
+for(let i =1; i<10;i++){
+    console.log(i)
+}
+
+let i = 0
+while(i<10){
+    console.log("value "+i)
+    i++
+}
+
+
+
+ar.forEach(
+    element => {
+        console.log(`this is the current element ${element }`)
+    }
+);
+
+
+console.log(1+'string')
+console.log(3-'string')
+console.log(5*'string')
+console.log(1/'string')
+console.log('-'.repeat(50))
+
+console.log('string'*10)
+console.log('string'+4)
+console.log('string'-3)
+console.log('string'/2)
+
+console.log('empty'.split('p'))
+console.log('EmptyParameterDumptyParamenter'.replace('m','k',2))
+console.log('emptythhs'[1,4])
+
+/*
+this is the comment 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
